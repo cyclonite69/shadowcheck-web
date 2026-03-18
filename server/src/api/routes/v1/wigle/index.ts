41be0afe520a708712758fa6a661a93fab66d664
Cyclonite01 <cyclonite01@users.noreply.github.com>
2026-03-17 23:57:41 -0400

    fix(api): standardized wigle route mounting to fix nested paths

--- server/src/api/routes/v1/wigle/index.ts
+++ server/src/api/routes/v1/wigle/index.ts
@@ -16,12 +16,12 @@ import observationsRoutes from './observations';
 import statsRoutes from './stats';
 
 // Mount routes
-router.use('/wigle', liveRoutes);
-router.use('/wigle', statusRoutes);
-router.use('/wigle', databaseRoutes);
-router.use('/wigle', searchRoutes);
-router.use('/wigle', detailRoutes);
-router.use('/wigle', observationsRoutes);
-router.use('/wigle', statsRoutes);
+router.use('/', liveRoutes);
+router.use('/', statusRoutes);
+router.use('/', databaseRoutes);
+router.use('/', searchRoutes);
+router.use('/', detailRoutes);
+router.use('/', observationsRoutes);
+router.use('/', statsRoutes);
 
 export default router;
