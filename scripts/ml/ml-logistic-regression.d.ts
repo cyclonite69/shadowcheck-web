2e197a1904d2769af5391b008e6a5ba46ffc0fb6
Cyclonite01 <cyclonite01@users.noreply.github.com>
2026-03-25 06:08:44 -0400

    chore: fix strict mode errors in scripts/ml and scripts/geocoding

---
+++ scripts/ml/ml-logistic-regression.d.ts
@@ -0,0 +1,5 @@
+declare module 'ml-logistic-regression' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const LogisticRegression: any;
+  export default LogisticRegression;
+}
